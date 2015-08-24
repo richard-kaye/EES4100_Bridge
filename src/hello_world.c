5283e4226f91f0794002dee6e64de690b3914d23
Kim Taylor <kim.taylor4@live.vu.edu.au>
2015-08-24 14:35:38 +1000

    Tidy up add_to_list()

--- src/hello_world.c
+++ src/hello_world.c
@@ -20,30 +20,38 @@ static pthread_cond_t list_data_flush = PTHREAD_COND_INITIALIZER;
 /* Add object to list */
 static void add_to_list(char *word) {
     word_object *last_object, *tmp_object;
-
-    char *tmp_string = strdup(word);
+    char *tmp_string;
+    
+    /* Do all memory allocation outside of locking - strdup() and malloc() can
+     * block */
     tmp_object = malloc(sizeof(word_object));
+    tmp_string = strdup(word);
+
+    /* Set up tmp_object outside of locking */
+    tmp_object->word = tmp_string;
+    tmp_object->next = NULL;
 
     pthread_mutex_lock(&list_lock);
 
     if (list_head == NULL) {
-	last_object = tmp_object;	
-	list_head = last_object;
+	/* The list is empty, just place our tmp_object at the head */
+	list_head = tmp_object;
     } else {
+	/* Iterate through the linked list to find the last object */
 	last_object = list_head;
 	while (last_object->next) {
 	    last_object = last_object->next;
 	}
+	/* Last object is now found, link in our tmp_object at the tail */
 	last_object->next = tmp_object;
-	last_object = last_object->next;
     }
-    last_object->word = tmp_string;
-    last_object->next = NULL;
 
     pthread_mutex_unlock(&list_lock);
     pthread_cond_signal(&list_data_ready);
 }
 
+/* Retrieve the first object in the linked list. Note that this function must
+ * be called with list_lock held */
 static word_object *list_get_first(void) {
     word_object *first_object;
 
@@ -69,10 +77,13 @@ static void *print_func(void *arg) {
 
 	pthread_mutex_unlock(&list_lock);
 
+	/* printf() and free() can block, make sure that we've released
+	 * list_lock first */
 	printf("Print thread: %s\n", current_object->word);
 	free(current_object->word);
 	free(current_object);
 
+	/* Let list_flush() know that we've done some work */
 	pthread_cond_signal(&list_data_flush);
     }
 
